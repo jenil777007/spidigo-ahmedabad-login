12723a43cc0bdb82464d9446f682c2d063f82721
jenil777007 <jenilcalcuttawala@gmail.com>
2016-07-19 00:52:10 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,33 +7,21 @@ class LoginTest(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get("http://spidigo.login/")
+        self.driver.get("http://spidigo.login/")    #Spidigo-URL
 
     def test_Login(self):
-        driver = self.driver
-        fbusername = "karanmodi"
-        fbpassword = "86610"
-        emailFieldID = "username"
-        passFieldID = "password"
-       # loginButtonXPath = "//input[@value='Log In']"
-        loginid = "btnLogin"
-      #  facebookLogoXPath =
+        driver          = self.driver
+        username        = "abc"                     #Your Spidigo-Username goes here
+        password        = "xyz@123"                 #Your Spidigo-Password goes here
+        usernameFieldID = "username"                #Text input id
+        passwordFieldID = "password"                #Text input id
+        loginid         = "btnLogin"                #Button id
 
-      #  efe = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(efe))
-       # pfe = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_id(pfe))
-        #loginButtonElement = WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_xpath(loginButtonXPath))
-
-        driver.find_element_by_id(emailFieldID).clear()
-        driver.find_element_by_id(emailFieldID).send_keys(fbusername)
-        driver.find_element_by_id(passFieldID).clear()
-        driver.find_element_by_id(passFieldID).send_keys(fbpassword)
+        driver.find_element_by_id(usernameFieldID).clear()
+        driver.find_element_by_id(usernameFieldID).send_keys(username)
+        driver.find_element_by_id(passwordFieldID).clear()
+        driver.find_element_by_id(passwordFieldID).send_keys(password)
         driver.find_element_by_id(loginid).click()
-
-       # efe.clear()
-       # efe.send_keys(fbusername)
-       # pfe.clear()
-       # pfe.send_keys(fbpassword)
-       # loginButtonElement.click()
 
     def tearDown(self):
         self.driver.quit()
